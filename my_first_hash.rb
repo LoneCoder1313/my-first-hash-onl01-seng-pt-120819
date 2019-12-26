ae474740b0d449506280fdf0ce7e2832e7b60e1d
Ricky dildy <rdildy92@gmail.com>
2019-12-26 22:21:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,11 +27,6 @@ def adding
                 "oil paintings" => 3
                  }
    shipping_manifest["muskets"]= 2 
-   
-   puts shipping_manifest={"whale bone corsets" =>  5, 
-                "porcelain vases" => 2, 
-                "oil paintings" => 3,
-                "muskets" =>  2}
                 
    shipping_manifest["gun powder"]= 4
    
