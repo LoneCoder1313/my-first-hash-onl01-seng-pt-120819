818532e7b77600767c49f637ee2558fbb3c204d0
Ricky dildy <rdildy92@gmail.com>
2019-12-26 22:24:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,8 +29,12 @@ def adding
    shipping_manifest["muskets"]= 2 
                 
    shipping_manifest["gun powder"]= 4
-   shipping_manifest= {
-   
+   shipping_manifest= { "whale bone corsets" => 5,
+                       "porcelain vases" => 2,
+                       "oil paintings" => 3,
+                       "muskets" => 2,
+                       "gun powder" => 4
+                       }
       puts shipping_manifest
    end 
    {"gun powder"=>4, "muskets"=>2, "oil paintings"=>3, "porcelainvases"=>2, "whale bone corsets"=>5}
