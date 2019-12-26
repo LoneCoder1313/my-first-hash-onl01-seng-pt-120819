8d59d629562772711242b64d8d2190ecf6f8a1e0
Ricky dildy <rdildy92@gmail.com>
2019-12-26 22:16:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,43 +1,50 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
-end
-
-
-def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
-end
-
+def  my_hash 
+          your_hash = { "my_name" => "Herman Melville"}
+          
+     end 
+   
+ def shipping_manifest  
+      the_manifest = {"whale bone corsets" =>  5, 
+                        "porcelain vases" => 2, 
+                        "oil paintings" => 3
+      }
+      old_timey_items = []
+              values_ar= [5,2,3]
+              keys_ar=["whale bone corsets", "porcelain vase", "oil paintings"]
+            if the_manifest.keys <=> keys_ar
+              while the_manifest.values <=> values_ar 
+              puts keys_ar|values_ar
+              ["whale bone corsets", 5, "porcelain vases", 2, "oil paintings"  ]
+              old_timey_items = [] <<  ["whale bone corsets", 5, "porcelain vases", 2, "oil paintings"  ]
+  end  
+end 
+end 
+        
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "whale bone corsets" =>  5, 
+                "porcelain vases" => 2, 
+                "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+          puts  shipping_manifest["oil paintings"]
+                 return 3
 end
-
+                
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                "whale bone corsets"  => 5, 
+                "porcelain vases" => 2, 
+                "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
-end
+   shipping_manifest["muskets"]= 2 
+   
+   puts shipping_manifest={"whale bone corsets" =>  5, 
+                "porcelain vases" => 2, 
+                "oil paintings" => 3,
+                "muskets" =>  2}
+                
+   shipping_manifest["gun powder"]= 4
+   
+      puts shipping_manifest 
+end 
+  
